aa538133f71d087c8b6b0c63dc88b740db94eff9
Tatiane Yukari <yukari.kawakami@hotmail.com>
2022-12-26 21:16:59 -0300

    Update files

--- main.py
+++ main.py
@@ -1,32 +1,79 @@
 from tkinter import *
+import math
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps = 1
 
+# ---------------------------- TIMER RESET ------------------------------- #
+
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start():
+    global reps
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps == 8:
+        count_down(long_break_sec)
+        title_label.config(text="Break", fg=RED)
+    elif reps % 2 != 0:
+        count_down(work_sec)
+        title_label.config(text="Working", fg=GREEN)
+    else:
+        count_down(short_break_sec)
+        title_label.config(text="Break", fg=PINK)
+
+    reps += 1
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+
+    if count_sec < 10:
+        canvas.itemconfig(timer_text, text=f"{count_min}:0{count_sec}")
+    else:
+        canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+
+
+    if count > 0:
+        window.after(1000,count_down, count - 1)
+    else:
+        start()
+
+
+
+# ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
-window.title("My first GUI Program")
-window.minsize(200, 200)
-window.config(padx=10, pady=50)
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW, )
 
-my_label = Label(text="is equal to")
-my_label.grid(row=1, column=0)
+title_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 25, "bold"))
+title_label.grid(column=1, row=0)
 
-miles = Label(text="Miles")
-miles.grid(row=0, column=2)
+button_start = Button(text="Start", command=start)
+button_start.grid(column=0, row=2)
 
-km = Label(text="Km")
-km.grid(row=1, column=2)
+button_reset = Button(text="Reset")
+button_reset.grid(column=2, row=2)
 
-def button_clicked():
-    num_miles = int(input.get())
-    num_km = int(num_miles * 1.61)
-    result = Label(text=num_km)
-    result.grid(row=1, column=1)
+check_label = Label(fg=GREEN)
+check_label.grid(column=1, row=3)
 
 
-button = Button(text="Calculate", command=button_clicked)
-button.grid(row=2, column=1)
-
-
-input = Entry(width=10)
-input.grid(row=0, column=1)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_image = PhotoImage(file="tomato.png")
+canvas.create_image(100,112, image=tomato_image)
+timer_text = canvas.create_text(100,130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
 
 
 
